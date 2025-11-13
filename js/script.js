05ae09c32a1cdcf044fbbb452ef117c93c6e0d49
Dante-AG1 <sololevtest@gmail.com>
2025-11-13 09:06:11 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ const CONFIG = {
     { name: "Ноутбук", deathDrop: true, weight: 10, repPerUnit: 12, price: 23000 },
     { name: "Системы наведения", deathDrop: true, weight: 0.4, repPerUnit: 16, price: 30000 },
     { name: "Блок данных «Бета»", deathDrop: false, weight: 0.2, repPerUnit: 45, price: 85000 },
-    { name: "Портативный квантовый генератор", deathDrop: false, weight: 3, repPerUnit: 50, price: 55000 },
+    { name: "Портативный квантовый генератор", deathDrop: false, weight: 3, repPerUnit: 50, price: 60000 },
     { name: "Цветущий рыжий папоротник", deathDrop: false, weight: 0.05, repPerUnit: 12, price: 14000 },
     { name: "Малый ящик с продовольствием", deathDrop: true, weight: 1, repPerUnit: 1, price: 2000 },
     { name: "Ящик с запчастями", deathDrop: true, weight: 2.5, repPerUnit: 1, price: 3000 },
